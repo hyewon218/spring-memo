37a9d241151d45ff18f759fcb6241f68a6651135
hyewon218 <won_11111@naver.com>
2023-06-14 18:14:34 +0900

    Create, Read 구현

---
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -0,0 +1,21 @@
+package com.sparta.memo.entity;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Memo {
+    // 메모끼리 구분하기 위해(데이터베이스에 필수로 필요한 개념)
+    private Long id;
+    private String username;
+    private String contents;
+
+    public Memo(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+}
